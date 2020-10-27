d8cdac350badd4875ef016034b4c19a8bf794db1
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2020-10-27 22:29:12 +0900

    create dataset class

---
+++ DLfromS3/steps/step49.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import dezero
+import math
+import numpy as np
+from dezero import optimizers
+import dezero.functions as F
+from dezero.models import MLP
+
+# ハイパーパラメータの設定
+max_epoch = 300
+batch_size = 30
+hidden_size = 10
+lr = 1.0
+
+# 以下step48.pyの改良
+train_set = dezero.datasets.Spiral()
+
+model = MLP((hidden_size, 3))
+optimizer = optimizers.SGD(lr).setup(model)
+
+data_size = len(train_set)
+max_iter = math.ceil(data_size / batch_size) # 小数点の切り上げ
+
+for epoch in range(max_epoch):
+  index = np.random.permutation(data_size)
+  sum_loss = 0
+
+  for i in range(max_iter):
+    batch_index = index[i * batch_size:(i+1) *batch_size]
+    # 以下step48.pyの改良
+    batch = [train_set[i] for i in batch_index]
+    batch_x = np.array([example[0] for example in batch])
+    batch_t = np.array([example[1] for example in batch])
+
+    y = model(batch_x)
+    loss = F.softmax_cross_entropy(y, batch_t)
+    model.cleargrads()
+    loss.backward()
+    optimizer.update()
+
+    sum_loss += float(loss.data) * len(batch_t)
+
+  avg_loss = sum_loss / data_size
+  print('epoch %d, loss %.2f'%(epoch+1, avg_loss))
